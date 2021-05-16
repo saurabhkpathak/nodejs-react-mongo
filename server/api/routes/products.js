466b46dba3b4dca319b65746b8993d49a9413179
Saurabh Pathak <pathaks@tcd.ie>
2021-05-16 22:49:57 +0100

    adding image upload feature to add product endpoint

--- server/api/routes/products.js
+++ server/api/routes/products.js
@@ -1,13 +1,40 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const multer = require('multer');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, new Date().toISOString() + file.originalname);
+    }
+});
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+        // can throw error
+        // cb(new Error(), true);
+    } else {
+        cb(null, false);
+    }
+}
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
     Product
         .find()
-        .select('name price _id')
+        .select('name price _id productImage')
         .exec()
         .then(docs => {
             res.status(200).json({
@@ -16,7 +43,8 @@ router.get('/', (req, res, next) => {
                     return {
                         name: x.name,
                         price: x.price,
-                        id: x._id
+                        id: x._id,
+                        productImage: x.productImage
                     }
                 })
             });
@@ -29,11 +57,13 @@ router.get('/', (req, res, next) => {
         });
 });
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('productImage'), (req, res, next) => {
+    console.log(req.file);
     const product = new Product({
         _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
-        price: req.body.price
+        price: req.body.price,
+        productImage: req.file.path
     });
     product
         .save()
